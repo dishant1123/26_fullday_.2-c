7291dd2269ce04de36ebc09401e4e3d26d036e0e
dishant1123 <dishantshah1417@gmail.com>
2026-04-17 15:41:05 +0530

    .....

--- switch/17april.c
+++ switch/17april.c
@@ -3,7 +3,7 @@
 /*
 ask  user to enter the character and check it  it is  vowel  consonant  or  digit or special character using  switch case. 
 */
-
+/*
 #include <stdio.h>
 int  main()
 {
@@ -38,6 +38,7 @@ int  main()
     }
     return 0; 
 }
+*/
 /*
 ======= calculation or  grade ===== 
 1.calculation 
@@ -50,3 +51,136 @@ case 2.
     a. enter the 3 subject marks and  print grade 
     b. enter the character and check ==> vowel consonant digit special character 
 */
+
+// if else : 
+
+/*
+    if (condition)
+{   
+    // code
+    }
+else 
+{
+    code 
+}
+*/
+
+// task :1  check  odd  or even 
+/*
+#include <stdio.h>
+int main()
+{
+    int num; 
+    printf("enter the  number  : \n"); 
+    scanf("%d",&num);
+
+    if(num % 2==0)
+    {
+        printf("it is even\n");
+    }
+    else 
+    {
+        printf("it is odd\n");
+    }
+    return 0 ;
+}
+    */
+// ask user to enter the age  and check whether its is eligible  for vote or not. 
+/*
+#include <stdio.h>
+int main()
+{
+    int age; 
+    printf("enter the  age : \n"); 
+    scanf("%d",&age);
+
+    if(age >18) 
+    {
+        printf("eligible  for voting  \n");
+    }
+    else 
+    {
+        printf("not eligible for voting\n");
+    }
+    return 0 ;
+}
+*/ 
+
+// ask user enter the year  and check it  is  leap yr or not. 
+
+// nested if : 
+/*
+#include <stdio.h>
+int main()
+{
+    int a,b; 
+    printf("enter the number  1 : \n");
+    scanf("%d",&a);
+    printf("enter the number  2 : \n");
+    scanf("%d",&b);
+
+    if(a>b)
+    {
+        printf("a is big"); 
+    }
+    else if(b>a)
+    {
+        printf("b is  big"); 
+    }
+    else 
+    {
+        printf("same");
+    }
+    return 0; 
+}
+*/ 
+
+// grade calculation  : 
+
+#include <stdio.h>
+int main()
+{
+    int phy,maths,che; 
+    float per,total; 
+
+    printf("enter the  phy marks : \n");
+    scanf("%d",&phy);
+    printf("enter the  maths marks : \n");
+    scanf("%d",&maths);
+    printf("enter the  che marks : \n");
+    scanf("%d",&che);
+    total = phy + maths + che;
+    per = total /3;
+    printf("your per is  : %f\n",per);
+
+    if(per >=90)
+    {
+        printf("A+");
+    }
+    else if(per >=80  && per <90)
+    {
+        printf("A");
+    }
+    else if(per >=70  && per <80)
+    {
+        printf("B+");
+    }
+    else if(per >=60  && per <70)
+    {
+        printf("B");
+    }
+    else if(per >=50  && per <60)
+    {
+        printf("C+");
+    }
+    else if(per >=40  && per <50)
+    {
+        printf("C");
+    }
+    else 
+    {
+        printf("fail");
+    }
+
+    return 0;
+}
