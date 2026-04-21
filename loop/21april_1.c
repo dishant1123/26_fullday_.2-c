d2fd7c8cb138ece1ecbcc87fef395f6fc4eab7fa
dishant1123 <dishantshah1417@gmail.com>
2026-04-21 14:46:39 +0530

    ....

--- loop/21april_1.c
+++ loop/21april_1.c
@@ -29,6 +29,7 @@ int  main()
 */ 
 
 // ex :100 -1 print 
+/*
 #include <stdio.h>
 int  main()
 {
@@ -40,5 +41,84 @@ int  main()
 
     return 0; 
 }
+*/
 
 // ex : 3 print odd number   between 1-100  .   ==> 1  3  5 7 9 ..99 
+/*
+#include <stdio.h>
+int  main()
+{
+    int i; 
+    for(i=1; i<=100; i+=2) //     3   3 <=100 i =i +2    i+=2   
+    {
+        printf("%d\t",i); // 1  3 
+    }
+
+    return 0; 
+}
+*/
+
+// ex :4  print  even number  between 1-100 . 
+
+/*
+#include <stdio.h>
+int  main()
+{
+    int i; 
+    for(i=2; i<=100; i+=2) //     3   3 <=100 i =i +2    i+=2   
+    {
+        printf("%d\t",i); // 1  3 
+    }
+
+    return 0; 
+}
+*/ 
+
+// ex :5 ask user to enter the ending  number . 
+/*
+#include<stdio.h>
+int main()
+{
+    int i,n; 
+    printf("enter the ending number : \n");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\t",i); 
+    }
+    return 0; 
+}
+*/ 
+// ex :6 ask user to enter the   ending  number  and print  odd even  number. 
+
+
+// ex :7  ask user  to enter the  number and  print  n natural  number  sum. 
+/*
+input  : 5 
+output  : 1+2+3+4+5 = 15
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i,n,sum=0; 
+    printf("enter the  num : "); 
+    scanf("%d",&n);  // 5 
+    
+    for(i=1; i<=n;i++) // i =6  6 <=5   // start  con  inc/dec 
+    {
+        sum =sum +i;   // sum =15
+    }
+
+    printf("sum is : %d\n",sum);
+    return 0; 
+}
+
+// ex :8 ask user to enter the  number  and  print  odd and  even  number  sum. 
+/*
+
+input  : 10 
+output  : even number  sum  : 2+4+6+8+10 =30 
+          odd number  sum  : 1+3+5+7+9  = 25
+*/
