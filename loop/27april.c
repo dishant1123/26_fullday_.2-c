8613c74a5320845a915818fb166eabb0dacabbee
dishant1123 <dishantshah1417@gmail.com>
2026-04-27 14:17:12 +0530

    ....

--- loop/27april.c
+++ loop/27april.c
@@ -4,7 +4,7 @@ strong  number  : 145
           sum = 1+120 +24 =145    
 
 */
-
+/*
 #include <stdio.h>
 int main()
 {
@@ -30,6 +30,7 @@ int main()
     }
     return 0; 
 }
+*/
 /*
 task :1 ask user  to enter the  number  and  print  table  of this  number.
 input  : 5 
@@ -37,5 +38,65 @@ ouput  : 5 * 1= 5
         5 * 2= 10
         5 * 3= 15
         5 *10 =50 
+
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int num,i; 
+    printf("enter the  number  : ");
+    scanf("%d",&num); 
+
+    for(i=1; i<=10; i++)
+    {
+        printf("%d * %d = %d\n",num,i,(num *i));                 // 5 * 1 = 5  
+    }
+    return 0; 
+}
+*/
+// nested loop  : 
+/*
+for()
+{
+    for() 
+}    
+*/
+/*
+task  :2  ask user to enter the  start  and  ending  number  and  print  prime  number  between two range. 
+
+input  start  : 20 
+input   end   : 100    23 ,29 .. 97 
+*/
+#include <stdio.h>
+int main()
+{
+    int start ,end, i,j,count,temp =0; 
+    printf("enter the start number : ");
+    scanf("%d",&start); // 5 
+    printf("enter the end number : ");
+    scanf("%d",&end); // 100 
+    for(i=start; i<=end; i++) // i =7  7 <=100  
+    {
+        count =0 ; // 0 
+        for(j=1; j<=i; j++) // j=1  1 <= 7  
+        {
+            if(i % j==0 )  // 5 % 5 ==0 
+            {
+                count ++;  // 4 
+            }
+        }
+        if(count ==2) // 4 ==2
+        {
+            printf("%d\t",i);//5 
+            temp ++; 
+        }
+    }
+        printf("\ntotal prime  number  between range  : %d\n",temp);
+    return 0 ;
+}
+
+/*
+perfect number  in range : 
 
 */
