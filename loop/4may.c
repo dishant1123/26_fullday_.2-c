330c09042d2582cf4faadf0927480f0ef42d2587
dishant1123 <dishantshah1417@gmail.com>
2026-05-04 14:26:07 +0530

    ....

--- loop/4may.c
+++ loop/4may.c
@@ -10,11 +10,13 @@
 row = 5
 col = 5 
 */
-
+/*
 #include <stdio.h>
-int  main()
+// #include <conio.h>
+int  main()  // void main 
 {
     int i,j;
+     // clrscr(); 
     for(i=1; i<=5; i++)  // rows print   i =  <
     {
         for(j=1; j<=i; j++) // j=   <= 
@@ -30,13 +32,62 @@ int  main()
         }                     // 1 2   
         printf("\n");         // 1 2 3 
     }                        //  1 2 3 4  
-    return 0;                //  1 2 3 4 5 
+    return 0;    // getch();              //  1 2 3 4 5 
+}
+*/ 
+/*                 
+1.                2.            3. 
+0                 1 1 1 1 1     1 2 3 4 5
+0 1               0 0 0 0       2 3 4 5
+0 1 0             1 1 1         3 4 5 
+0 1 0 1           0 0           4 5 
+0 1 0 1 0         1             5 
+*/
+/*
+#include <stdio.h>
+// #include <conio.h>
+int  main()  // void main 
+{
+    int i,j;
+    for(i=5; i>=1; i--)   // 5   4 >=1 
+    {
+        for(j=1; j<=i; j++)  // j=2   2 <=4  
+        {
+            if(i % 2==0)     // 4 %2 ==0 
+            {
+                printf("0 ");  
+            }
+            else 
+            {
+                printf("1 ");   // 1 1 1 1 1 
+            }                  //  0 0 
+        }                        
+        printf("\n");          
+    }                          
+    return 0;    
 }
 
-/*                 
-0                 1 1 1 1 1 
-0 1               0 0 0 0 
-0 1 0             1 1 1
-0 1 0 1           0 0
-0 1 0 1 0         1
 */
+/*
+
+1 2 3 4 5      5 4 3 2 1
+2 3 4 5        5 4 3 2
+3 4 5          5 4 3
+4 5            5 4
+5              5
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j; 
+    for(i=1; i<=5; i++)  // i =3  3 <=5 
+    {
+        for(j=i; j<=5; j++) // j=5   5<=5 
+        {
+            printf("%d ",j); // 1 2 3 4 5  
+        }                   //  2 3 4 5 
+        printf("\n");       //  3 4 5 
+    }
+    return 0; 
+}
